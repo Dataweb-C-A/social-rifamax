04508cd62774383ccd6b487ddff23c7531946cb5
EvananSemprun <evanansemprun@gmail.com>
2024-05-21 18:13:23 -0400

    cedula found

--- src/pages/Coupon.tsx
+++ src/pages/Coupon.tsx
@@ -1,11 +1,12 @@
-import { Breadcrumbs, Anchor, Card, createStyles, Title, Group, Text, Stepper, ScrollArea, Button, Divider } from "@mantine/core"
+import { Breadcrumbs, Anchor, Card, createStyles,Select,ActionIcon, TextInput,Title, Group, Text, Stepper, ScrollArea, Button, Divider } from "@mantine/core"
 import Layout from "../Layout"
 import PublicNavbar from "../components/PublicNavbar"
 import { useNavigate } from "react-router-dom"
 import { useMediaQuery } from '@mantine/hooks';
 import { useState, useRef } from "react";
+import axios from "axios"
 import { motion } from 'framer-motion'
-import { IconCreditCard, IconMinus, IconPlus, IconShoppingBag, IconUser } from "@tabler/icons-react";
+import { IconCreditCard, IconSearch, IconShoppingBag, IconUser } from "@tabler/icons-react";
 
 interface ICoupon {
 
@@ -18,7 +19,7 @@ function Coupon({ }: ICoupon) {
     main: {},
     mainCard: {
       margin: '5px 5px 0 5px',
-      height: 'calc(100vh - 85px)',
+      height: '95vh',
       boxShadow: `0 1px 0 0 ${theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[2]}`,
       background: `${theme.colorScheme === 'dark' ? '#1A1B1E' : '#fff'}`,
       border: `0.0625rem solid ${theme.colorScheme === 'dark' ? 'transparent' : '#dee2e6'}`,
@@ -38,7 +39,7 @@ function Coupon({ }: ICoupon) {
       justifyContent: 'center',
       alignItems: 'center',
       width: '17%',
-      height: '130px',
+      height: '100px',
       ':hover': {
         background: theme.colors.blue[5],
         cursor: 'pointer',
@@ -64,7 +65,7 @@ function Coupon({ }: ICoupon) {
       border: `0.0625rem solid ${theme.colorScheme === 'dark' ? 'transparent' : '#dee2e6'}`,
       borderBottom: 'none',
       marginTop: '10px',
-      height: '95%',
+      height: '100%',
       bottom: 0,
       zIndex: 9999,
       borderRadius: '25px 25px 0 0',
@@ -141,6 +142,63 @@ function Coupon({ }: ICoupon) {
   )
 
   const Steps = () => {
+    const [openedinfi, setOpenedinfi] = useState(false);
+  const [nombre, setNombre] = useState('');
+  const [apellido, setApellido] = useState('');
+  const [tipoDocumento, setTipoDocumento] = useState('V-');
+  const [cedula, setCedula] = useState('');
+  const [prefijo, setPrefijo] = useState('');
+  const [numeroTelefono, setNumeroTelefono] = useState('');
+
+
+
+  const [clientId, setClientId] = useState(null);
+
+  const handleclienteClick = () => {
+    const cliente = {
+      x100_client: {
+        name: `${nombre} ${apellido}`,
+        phone: `+58 ${prefijo} ${numeroTelefono}`,
+        dni: `${tipoDocumento}${cedula}`
+      }
+    };
+
+    axios.post('https://api.rifa-max.com/x100/clients', cliente)
+      .then(response => {
+        console.log('Datos enviados correctamente:', response.data);
+        setOpenedinfi(true);
+        setClientId(response.data?.user?.id);
+      })
+      .catch(error => {
+        console.error('Error al enviar los datos:', error);
+      });
+
+
+  };
+
+  const [visible, setvisible] = useState(false);
+
+  const handleActionIconClick = () => {
+    axios.get(`https://api.rifa-max.com/x100/clients/dni?dni=${tipoDocumento}${cedula}`)
+      .then(response => {
+        const data = response.data;
+        if (data && data.id) {
+          setClientId(data.id);
+          setNumeroTelefono(data.phone);
+          setNombre(data.name);
+          setvisible(false);
+        }
+        setOpenedinfi(true);
+      })
+      .catch(error => {
+        setvisible(true);
+        setNombre('');
+        setNumeroTelefono('');
+        console.error('Error al obtener los datos:', error);
+      });
+  };
+
+
     return (
       <Stepper size='xs' mt={20} px={10} active={active} onStepClick={setActive}>
         <Stepper.Step
@@ -181,13 +239,127 @@ function Coupon({ }: ICoupon) {
               Total a pagar: {quantity * 21}$
             </Text>
           </Group>
+          <Button onClick={nextStep}>Next step</Button>
         </Stepper.Step>
         <Stepper.Step
           icon={<IconUser size={16} />}
           label={<Text mt={3} fz={10}>Información</Text>}
           description={<Text mt={-3} fz={7} c="dimmed">Datos personales</Text>}
         >
-          Step 2 content: Verify email
+            <Text ta="center" mt={5} fw={750}>Ingrese sus datos</Text>
+
+<Text mt={5} fz={14}>
+  Cédula o DNI
+</Text>
+<Group>
+  <Select
+    w={120}
+    mt={10}
+    radius="md"
+    size="md"
+    value={tipoDocumento}
+    onChange={(value) => {
+      if (value !== null) {
+        setTipoDocumento(value);
+      }
+    }}
+    data={[
+      { value: 'V-', label: 'V' },
+      { value: 'E-', label: 'E' },
+      { value: 'J-', label: 'J' },
+      { value: 'G-', label: 'G' },
+    ]}
+  />
+  <TextInput
+    placeholder="Cédula o DNI"
+    mt={10}
+    w={370}
+    size="md"
+    type="number"
+    maxLength={8}
+    radius="md"
+    value={cedula}
+    onChange={(e) => setCedula(e.target.value)}
+  />
+
+  <ActionIcon  color="lime" mt={5} size="xl" radius="lg" variant="filled" onClick={handleActionIconClick}>
+    <IconSearch size={30} />
+  </ActionIcon>
+
+</Group>
+
+{visible && (
+  <Group spacing={5}>
+    <TextInput
+      radius="md"
+      style={{ width: '49.2%' }}
+      label='Nombre'
+      placeholder="Nombre"
+      value={nombre}
+      onChange={(e) => setNombre(e.target.value)}
+    />
+    <TextInput
+      radius="md"
+      style={{ width: '49.2%' }}
+      label='Apellido'
+      placeholder="Apellido"
+      value={apellido}
+      onChange={(e) => setApellido(e.target.value)}
+    />
+  </Group>
+)}
+
+
+
+{visible && (
+  <Group mt={10}>
+
+    <Select
+      label="Prefijo"
+      placeholder="xxx"
+      size="md"
+      radius="md"
+      style={{ width: '40%' }}
+      data={[
+        { value: '(412)', label: '412' },
+        { value: '(416)', label: '416' },
+        { value: '(414)', label: '414' },
+        { value: '(424)', label: '424' },
+        { value: '(426)', label: '426' },
+      ]}
+      value={prefijo}
+      onChange={(value) => {
+        if (value !== null) {
+          setPrefijo(value);
+        }
+      }}
+    />
+
+    <TextInput
+      color="red"
+      label={<Text>Número telefónico</Text>}
+      placeholder="xxx-xxxx"
+      style={{ width: '55%' }}
+      size="md"
+      radius="md"
+      maxLength={8}
+      value={numeroTelefono}
+      onChange={(e) => {
+        const inputValue = e.target.value;
+        const numericValue = inputValue.replace(/\D/g, '');
+        let formattedValue = numericValue;
+        if (numericValue.length > 3) {
+          formattedValue = numericValue.substring(0, 3) + '-' + numericValue.substring(3);
+        }
+        if (numericValue.length > 7) {
+          formattedValue = numericValue.substring(0, 7);
+        }
+        setNumeroTelefono(formattedValue);
+      }}
+    />
+
+  </Group>
+)}
         </Stepper.Step>
         <Stepper.Step
           icon={<IconCreditCard size={16} />}
@@ -205,7 +377,6 @@ function Coupon({ }: ICoupon) {
 
   return (
     <section className={classes.main}>
-      <PublicNavbar />
       <Layout full noOverlap>
         <Card className={classes.mainCard}>
           <SuperiorHead />
